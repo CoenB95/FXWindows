c22282f9a69209f03a43a2d800188782b89e2e11
Coen <boelhouwers.coen@gmail.com>
2016-11-13 16:22:50 +0100

    Removed old Canvas code.

--- FXWindows/src/fxwindows/Main.java
+++ FXWindows/src/fxwindows/Main.java
@@ -8,19 +8,12 @@ import com.cbapps.javafx.utilities.resources.RobotoFont;
 
 import fxwindows.animation.Animation;
 import fxwindows.animation.MoveAnimation;
-import fxwindows.drawable.Container;
-import fxwindows.drawable.ListView;
-import fxwindows.drawable.Texie;
+import fxwindows.wrapped.Container;
 import fxwindows.wrapped.ListContainer;
+import fxwindows.wrapped.Texie;
 import fxwindows.wrapped.WrappedRectangle;
-import javafx.geometry.VPos;
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
-import javafx.scene.text.Text;
-import javafx.scene.text.TextAlignment;
 
 public class Main extends Manager {
 
@@ -30,76 +23,9 @@ public class Main extends Manager {
 	
 	@Override
 	public void setup(Container canvas) {
-		Texie texie = new Texie("Welkom op Avans Hogeschool Breda");
-		texie.wrappingWidthProperty().bind(canvas.transformedWidthProperty()
-				.multiply(0.8));
-		texie.setFont(Font.loadFont(RobotoFont.thin(),120));
-		texie.setTextColor(Color.WHITE);
-		//text.setTextAlignment(TextAlignment.CENTER);
-		texie.bindX(canvas.transformedWidthProperty().divide(2)
-				.subtract(texie.transformedWidthProperty().divide(2)));
-		texie.bindY(canvas.transformedHeightProperty().divide(2)
-				.subtract(texie.transformedHeightProperty().divide(2)));
-		
-		ListView texie2 = new ListView();
-		texie2.setXY(50, 50);
-		texie2.setGeneralFont(Font.loadFont(RobotoFont.medium(), 24));
-		for (int i = 0;i < 20;i++) {
-			texie2.addTextItem("Test item "+i);
-		}
-		
-		/*ListView list2 = new ListView();
-		list2.setY(50);
-		list2.bindX(texie2.transformedXProperty().add(texie2.transformedWidthProperty()).add(10));
-		list2.setGeneralFont(Font.loadFont(RobotoFont.medium(), 24));
-		for (int i = 0;i < 20;i++) {
-			list2.addTextItem("Test item "+i);
-		}
-		
-		ListView list3 = new ListView();
-		list3.setY(50);
-		list3.bindX(list2.transformedXProperty().add(texie2.transformedWidthProperty()).add(10));
-		list3.setGeneralFont(Font.loadFont(RobotoFont.medium(), 24));
-		for (int i = 0;i < 20;i++) {
-			list3.addTextItem("Test item "+i);
-		}*/
-
-		canvas.setOnMouseClicked(() -> {
-			texie.setAlpha(1);
-			MoveAnimation b = new MoveAnimation(texie, Duration.ofSeconds(1));
-			b.setFromY(canvas.getHeight()/2 + texie.getHeight()/2);
-			b.setInterpolator(new SmoothInterpolator(AnimType.DECELERATE));
-			b.start();
-			MoveAnimation c = new MoveAnimation(texie, Duration.ofSeconds(1));
-			c.setToX(texie.getWidth()*-1.2);
-			c.setInterpolator(new SmoothInterpolator(AnimType.ACCELERATE));
-			c.startAt(3000);
-			new Animation(texie2, Duration.ofMillis(4500)) {	
-				@Override
-				public void update(double progress) {
-					texie2.scroll = -(texie2.getListHeight()-texie2.getHeight()) *
-							progress;
-				}
-			}.startAt(500);
-			new Animation(texie2, Duration.ofMillis(500)) {	
-				@Override
-				public void update(double progress) {
-					texie2.scroll = -(texie2.getListHeight()-texie2.getHeight()) *
-							(1.0-progress);
-				}
-			}.start();
-		});
-		setBackgroundColor(Color.DARKSLATEBLUE);
-		canvas.getChildren().addAll(texie, texie2);//, list2, list3);
-	}
-	
-	@Override
-	public void setupShape(fxwindows.wrapped.Container canvas) {
 		WrappedRectangle background = new WrappedRectangle(
 				canvas.widthProperty(), canvas.heightProperty());
 		background.setBackgroundColor(Color.DARKSLATEBLUE);
-		//background.widthProperty().bind(canvas.widthProperty());
-		//background.heightProperty().bind(canvas.heightProperty());
 		
 		fxwindows.wrapped.Texie texie = new fxwindows.wrapped.Texie(
 				"Welkom op Avans Hogeschool Breda");
@@ -114,37 +40,25 @@ public class Main extends Manager {
 				texie.heightProperty().divide(2)));
 		//texie.setTextOrigin(VPos.CENTER);
 		
-		
-		//texie.addToStage(canvas);
-		
-		ListContainer texie2 = new ListContainer();
-		texie2.setXY(100, 10);
-		texie2.setBackgroundColor(Color.RED);
+		ListContainer list1 = new ListContainer();
+		list1.setXY(100, 10);
+		list1.setBackgroundColor(Color.RED);
 		Font f = Font.loadFont(RobotoFont.medium(), 24);
-		//texie2.setGeneralFont(Font.loadFont(RobotoFont.medium(), 24));
 		for (int i = 0;i < 20;i++) {
-			//texie2.addTextItem("Test item "+i);
-			fxwindows.wrapped.Texie t = new fxwindows.wrapped.Texie("Test "+i,f);
+			Texie t = new Texie("Test "+i,f);
 			t.setTextColor(Color.WHITE);
-			texie2.getChildren().add(t);
+			list1.getChildren().add(t);
 		}
 		
 		ListContainer list2 = new ListContainer();
-		list2.setY(50);
-		list2.bindX(texie2.transformedXProperty().add(texie2.widthProperty()));
-		//list2.setGeneralFont(Font.loadFont(RobotoFont.medium(), 24));
+		list2.setBackgroundColor(Color.PURPLE);
+		list2.bindY(list1.transformedYProperty());
+		list2.bindX(list1.transformedXProperty().add(list1.widthProperty()));
 		for (int i = 0;i < 20;i++) {
-			list2.getChildren().add(new fxwindows.wrapped.Texie("Test item "+i, f,
-					Color.WHITE));
+			list2.getChildren().add(new Texie("Test item "+i, f, Color.WHITE));
 		}
 		
-		WrappedRectangle testrect = new WrappedRectangle(10, 10);
-		testrect.setY(10);
-		testrect.setBackgroundColor(Color.ALICEBLUE);
-		testrect.bindX(texie2.transformedXProperty());//.add(texie2.widthProperty()));
-		//testrect.bindWidth(texie2.widthProperty());
-		
-		canvas.getChildren().addAll(background, texie, texie2, list2, testrect);
+		canvas.getChildren().addAll(background, texie, list1, list2);
 
 		canvas.setOnMouseClicked(() -> {
 			texie.setAlpha(1);
@@ -156,22 +70,20 @@ public class Main extends Manager {
 			c.setToX(texie.getWidth()*-1.2);
 			c.setInterpolator(new SmoothInterpolator(AnimType.ACCELERATE));
 			c.startAt(3000);
-			new Animation(texie2, Duration.ofMillis(4500)) {	
+			new Animation(list1, Duration.ofMillis(4500)) {	
 				@Override
 				public void update(double progress) {
-					texie2.setScroll( -(texie2.getListHeight()-texie2.getHeight()) *
+					list1.setScroll( -(list1.getListHeight()-list1.getHeight()) *
 							progress);
 				}
 			}.startAt(500);
-			new Animation(texie2, Duration.ofMillis(500)) {	
+			new Animation(list1, Duration.ofMillis(500)) {	
 				@Override
 				public void update(double progress) {
-					texie2.setScroll( -(texie2.getListHeight()-texie2.getHeight()) *
+					list1.setScroll( -(list1.getListHeight()-list1.getHeight()) *
 							(1.0-progress));
 				}
 			}.start();
 		});
-//		setBackgroundColor(Color.DARKSLATEBLUE);
-//		canvas.getChildren().addAll(texie, texie2);
 	}
 }
--- FXWindows/src/fxwindows/Manager.java
+++ FXWindows/src/fxwindows/Manager.java
@@ -1,19 +1,12 @@
 package fxwindows;
 
-import java.util.Collection;
-import fxwindows.drawable.Container;
-import fxwindows.drawable.Drawable;
+import fxwindows.wrapped.Container;
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
-import javafx.scene.paint.Paint;
-import javafx.scene.shape.Shape;
-import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -22,23 +15,14 @@ public abstract class Manager extends Application {
 
 	private AnimationTimer timer;
 	private Pane pane;
-	private Container container;
-	private fxwindows.wrapped.Container shapeContainer;
-	private Paint backgroundColor;
-	private Drawable selectedDrawable;
-	
-	private Canvas canvas;
+	private Container shapeContainer;
 	private long frameStart;
 	private long frameCount;
 	private long fps;
 	
-	private double mouseX;
-	private double mouseY;
-
 	@Override
 	public void start(Stage primaryStage) {
 		shapeVersion(primaryStage);
-		//canvasVersion(primaryStage);
 	}
 	
 	public void shapeVersion(Stage primaryStage) {
@@ -64,70 +48,10 @@ public abstract class Manager extends Application {
 		pane = new Pane();
 		pane.getChildren().add(canv);
 		shapeContainer = new DummyShapeContainer(pane);
-		setupShape(shapeContainer);
+		setup(shapeContainer);
 		pane.getChildren().add(t);
 		primaryStage.setScene(new Scene(new BorderPane(pane),500,500));
 		primaryStage.show();
-	}
-	
-	public void canvasVersion(Stage primaryStage) {
-		try {
-			pane = new Pane();			
-			canvas = new Canvas(400,400);
-			canvas.widthProperty().bind(pane.widthProperty());
-			canvas.heightProperty().bind(pane.heightProperty());
-			canvas.setOnMouseMoved((e) -> {
-				mouseX = e.getSceneX();
-				mouseY = e.getSceneY();
-			});
-			canvas.setOnMouseClicked((e) -> {
-				if (selectedDrawable != null) selectedDrawable.click();
-			});
-			GraphicsContext g = canvas.getGraphicsContext2D();
-
-			timer = new AnimationTimer() {
-
-				@Override
-				public void handle(long now) {
-					frameCount++;
-					if (System.currentTimeMillis() > frameStart + 250) {
-						fps = frameCount*4;
-						frameStart = System.currentTimeMillis();
-						frameCount = 0;
-					}
-					
-					// Update position (Animations)
-					container.update(now/100000);
-					
-					// Update selections (Mouse)
-					Drawable prev = selectedDrawable;
-					selectedDrawable = container.mouse(mouseX, mouseY);
-					if (prev != selectedDrawable)
-						System.out.println("Hovered: " + selectedDrawable);
-					
-					// Actually draw
-					g.setFill(backgroundColor);
-					g.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
-					container.draw(g);
-					g.setFill(Color.WHITE);
-					g.setFont(Font.getDefault());
-					g.fillText("FPS: " + fps,10,10);
-				}
-			};
-			timer.start();
-			
-			pane.getChildren().add(canvas);
-			
-			Scene scene = new Scene(new BorderPane(pane),400,400);
-			primaryStage.setScene(scene);
-			primaryStage.show();
-			
-			container = new DummyContainer(canvas);
-			setup(container);
-			
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
 	}
 	
 	private static class DummyShapeContainer extends fxwindows.wrapped.Container {
@@ -141,39 +65,6 @@ public abstract class Manager extends Application {
 			addToPane(canv);
 		}
 	}
-	
-	private static class DummyContainer extends Container {
-		public DummyContainer(Canvas canv) {
-			super();
-			bindHeight(canv.heightProperty());
-			bindWidth(canv.widthProperty());
-		}
-		@Override
-		public void draw(GraphicsContext graphics) {
-			for (Drawable child : getChildren()) {
-				child.draw(graphics);
-			}
-		}
-	}
-	
-	public void setBackgroundColor(Paint color) {
-		backgroundColor = color;
-	}
-
-
 
 	public abstract void setup(Container canvas);
-	public abstract void setupShape(fxwindows.wrapped.Container canvas);
-
-	public final void registerShape(Shape s) {
-		pane.getChildren().add(s);
-	}
-
-	public final void registerShapes(Shape... s) {
-		pane.getChildren().addAll(s);
-	}
-
-	public final void registerShapes(Collection<? extends Shape> s) {
-		pane.getChildren().addAll(s);
-	}
 }
