af00b1db5386c6ab8791dcb41a7921af021efd9d
Coen <boelhouwers.coen@gmail.com>
2017-04-06 13:17:14 +0200

    Cleaned up LogView.

--- src/fxwindows/view/LogView.java
+++ src/fxwindows/view/LogView.java
@@ -2,6 +2,7 @@ package fxwindows.view;
 
 import fxwindows.animation.*;
 import fxwindows.core.LayoutBehavior;
+import fxwindows.core.ShapeBase;
 import fxwindows.wrapped.Text;
 import fxwindows.wrapped.container.HorizontalContainer;
 import fxwindows.wrapped.container.VerticalContainer;
@@ -37,9 +38,11 @@ public class LogView extends VerticalContainer implements ListChangeListener<Log
 	private Font font;
 	private Color childBackground = Color.WHITE;
 	private Color childBorder = Color.LIGHTGRAY;
+	private AnimationType animtype;
 
-	public LogView(double textSize, Predicate<Log> filtr) {
+	public LogView(double textSize, AnimationType type, Predicate<Log> filtr) {
 		this.filter = filtr;
+		this.animtype = type;
 		this.textSize = textSize;
 		this.font = Font.font("Roboto", textSize);
 
@@ -48,6 +51,38 @@ public class LogView extends VerticalContainer implements ListChangeListener<Log
 		setMaxWidth(200);
 		setMaxHeight(158);
 		setWidthBehavior(LayoutBehavior.FILL_SPACE);
+	}
+
+	private Animation createEnterAnim(ShapeBase log) {
+		switch (animtype) {
+			default:
+			case ANIM_1:
+				return new ScaleAnimation(log, Duration.ofMillis(400), 1, 0, 1, 1)
+						.setInterpolator(new SmoothInterpolator(SmoothInterpolator.AnimType.DECELERATE));
+			case ANIM_2:
+				return new ScaleAnimation(log, Duration.ofMillis(400), 0, 0, 0, 1)
+						.setInterpolator(new SmoothInterpolator(SmoothInterpolator.AnimType.DECELERATE))
+						.then(new ScaleAnimation(log, Duration.ofMillis(400), 0, 1, 1, 1)
+								.setInterpolator(new SmoothInterpolator(SmoothInterpolator.AnimType.DECELERATE))
+						);
+		}
+	}
+
+	private Animation createExitAnim(ShapeBase log) {
+		switch (animtype) {
+			default:
+			case ANIM_1:
+				return new ScaleAnimation(log, Duration.ofMillis(400), 1, 1, 1, 0)
+						.setInterpolator(new SmoothInterpolator(SmoothInterpolator.AnimType.DECELERATE))
+								.then(() -> getChildren().remove(log));
+			case ANIM_2:
+				return new ScaleAnimation(log, Duration.ofMillis(400), 1, 1, 0, 1)
+						.setInterpolator(new SmoothInterpolator(SmoothInterpolator.AnimType.DECELERATE))
+						.then(new ScaleAnimation(log, Duration.ofMillis(400), 0, 1, 0, 0)
+								.setInterpolator(new SmoothInterpolator(SmoothInterpolator.AnimType.DECELERATE))
+								.then(() -> getChildren().remove(log))
+						);
+		}
 	}
 
 	public static Log log(String message, Color color) {
@@ -78,27 +113,10 @@ public class LogView extends VerticalContainer implements ListChangeListener<Log
 		log.setBorderColor(childBorder);
 		log.setWidthBehavior(LayoutBehavior.FILL_SPACE);
 		log.setScaleY(0);
-		Animation anim1 =
-				new ScaleAnimation(log, Duration.ofMillis(400), 0, 0, 0, 1)
-						.setInterpolator(new SmoothInterpolator(SmoothInterpolator.AnimType.DECELERATE))
-						.then(new ScaleAnimation(log, Duration.ofMillis(400), 0, 1, 1, 1)
-								.setInterpolator(new SmoothInterpolator(SmoothInterpolator.AnimType.DECELERATE))
-						);
-		/*new ValueAnimation(log.scaleYProperty(), Duration.ofMillis(400))
-				.setFrom(0)
-				.setTo(1)
-				.setInterpolator(new SmoothInterpolator(SmoothInterpolator.AnimType.DECELERATE));*/
 
-		Animation anim2 = /*new ValueAnimation(log.scaleYProperty(), Duration.ofMillis(400))
-						.setFrom(1)
-						.setTo(0)
-						.setInterpolator(new SmoothInterpolator(SmoothInterpolator.AnimType.DECELERATE))*/
-				new ScaleAnimation(log, Duration.ofMillis(400), 1, 1, 0, 1)
-						.setInterpolator(new SmoothInterpolator(SmoothInterpolator.AnimType.DECELERATE))
-						.then(new ScaleAnimation(log, Duration.ofMillis(400), 0, 1, 0, 0)
-								.setInterpolator(new SmoothInterpolator(SmoothInterpolator.AnimType.DECELERATE))
-								.then(() -> getChildren().remove(log))
-						);
+		Animation anim1 = createEnterAnim(log);
+		Animation anim2 = createExitAnim(log);
+
 		anim1.then(anim2, 3000);
 		anim2.pause(!log.getNode().isVisible(), 5000);
 		log.setPadding(2);
@@ -128,15 +146,8 @@ public class LogView extends VerticalContainer implements ListChangeListener<Log
 		progressCircle.setPadding(textSize / 5);
 		Animation fade = new FadeAnimation(progressCircle, Duration.ofMillis(1000))
 				.setFrom(1).setTo(0);
-		new ScaleAnimation(hor, Duration.ofMillis(400), 0, 0, 0, 1)
-				.setInterpolator(new SmoothInterpolator(SmoothInterpolator.AnimType.DECELERATE))
-				.then(new ScaleAnimation(hor, Duration.ofMillis(400), 0, 1, 1, 1)
-		//new ValueAnimation(hor.scaleYProperty(), Duration.ofMillis(400))
-		//		.setFrom(0)
-		//		.setTo(1)
-				.setInterpolator(new SmoothInterpolator(SmoothInterpolator.AnimType.DECELERATE))
-				)
-				.start();
+
+		createEnterAnim(hor).start();
 
 		logText.setPadding(5);
 		msg.progressProperty().addListener((v1, v2, v3) -> {
@@ -144,16 +155,7 @@ public class LogView extends VerticalContainer implements ListChangeListener<Log
 				Platform.runLater(() -> {
 					LOGS.remove(msg);
 					fade.start();
-					new ScaleAnimation(hor, Duration.ofMillis(400), 1, 1, 0, 1)
-							.setInterpolator(new SmoothInterpolator(SmoothInterpolator.AnimType.DECELERATE))
-							.then(new ScaleAnimation(hor, Duration.ofMillis(400), 0, 1, 0, 0)
-									.setInterpolator(new SmoothInterpolator(SmoothInterpolator.AnimType.DECELERATE))
-							).startAt(5000);
-//					new ValueAnimation(hor.scaleYProperty(), Duration.ofMillis(400))
-//							.setFrom(1)
-//							.setTo(0)
-//							.then(() -> Platform.runLater(() -> getChildren().remove(hor)))
-//							.startAt(5000);
+					createExitAnim(hor).startAt(5000);
 				});
 			}
 		});
@@ -264,59 +266,7 @@ public class LogView extends VerticalContainer implements ListChangeListener<Log
 		}
 	}
 
-	/*public static class ProgressLog {
-
-		private HorizontalContainer hor;
-		private DoubleProperty progress;
-		private Text log;
-		private ProgressCircle prog;
-
-		private ProgressLog(String message, Color color) {
-			hor = new HorizontalContainer();
-			hor.setBorderColor(Color.LIGHTGRAY);
-			hor.setWidthBehavior(LayoutBehavior.FILL_SPACE);
-			progress = new SimpleDoubleProperty();
-			log = new Text(message, Font.loadFont(RobotoFont.regular(), 12), color);
-			prog = ProgressCircle.small(Color.BLUE);
-			Animation fade = new FadeAnimation(prog, Duration.ofMillis(1000))
-					.setFrom(1).setTo(0);
-			ValueAnimation scale = new ValueAnimation(hor.scaleYProperty(), Duration.ofMillis(400))
-					.setFrom(0).setTo(1).setInterpolator(new SmoothInterpolator(SmoothInterpolator.AnimType.DECELERATE));
-			log.setPadding(5);
-			progress.addListener((v1, v2, v3) -> {
-				if (v3.doubleValue() >= 1) {
-					Platform.runLater(() -> {
-						fade.start();
-						scale.setFrom(1).setTo(0).then(() -> Platform.runLater(() -> getChildren().remove(hor)))
-								.startAt(5000);
-					});
-				}
-			});
-			hor.getChildren().addAll(log, prog);
-			Platform.runLater(() -> {
-				getChildren().add(hor);
-				scale.start();
-			});
-		}
-
-		public DoubleProperty progressProperty() {
-			return progress;
-		}
-
-		public void setColor(Color c) {
-			Platform.runLater(() -> {
-				log.setTextColor(c);
-				prog.setBorderColor(c);
-			});
-		}
-		public void setProgress(double value) {
-			progress.set(value);
-		}
-
-		public void setText(String value) {
-			Platform.runLater(() -> {
-				log.setText(value);
-			});
-		}
-	}*/
+	public enum AnimationType {
+		ANIM_1, ANIM_2
+	}
 }
